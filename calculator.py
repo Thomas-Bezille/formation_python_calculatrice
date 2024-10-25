6951f8395181d196011bd8f9a0bf2d391502e1b5
thomas <bezille.thomasb@gmail.com>
2024-10-25 16:06:41 +0200

    📦 NEW: Securing the variable to be sure to have a number

--- calculator.py
+++ calculator.py
@@ -2,7 +2,14 @@
 # créer une calculatrice en ligne de commande qui demande à l'utilisateur de saisir deux nombres
 # et qui affiche ensuite le résultat de l'addition de ces deux nombres.
 
-nbr1 = input("Entrez un premier nombre: ")
-nbr2 = input("Entrez un deuxième nombre: ")
-result = int(nbr1) + int(nbr2)
-print(f"Le résultat de l'addition de {nbr1} et {nbr2} est: {result}")
+while True:
+    nbr1 = input("Entrez un premier nombre: ")
+    nbr2 = input("Entrez un deuxième nombre: ")
+    
+    if not nbr1.isnumeric() or not nbr2.isnumeric():
+        print("Veuillez entrer des nombres entiers")
+        continue
+    else:
+        result = int(nbr1) + int(nbr2)
+        print(f"Le résultat de l'addition de {nbr1} et {nbr2} est: {result}")
+        break
